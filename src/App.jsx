31e2eed2572c80baa6207a3dea27f9c07c11b9d6
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-10-14 16:17:51 +0200

    Creation of the sidebar and integration in the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
     <div>
-      <Navbar />
-      {/* <section>Hero</section>
-      <section>Parallax</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
-      <section>Portfolio</section>
-      <section>Portfolio</section>
-      <section>Portfolio</section>
-      <section>Contact</section> */}
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-// import Sidebar from "../sidebar/Sidebar";
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
 
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
-      {/* <Sidebar/> */}
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
